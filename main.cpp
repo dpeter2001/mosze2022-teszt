4b82e302e5013042ff8d1217253933574af7dd3e
dpeter2001 <71820001+dpeter2001@users.noreply.github.com>
2022-09-13 22:05:42 +0200

    fix: fixing-errors

--- main.cpp
+++ main.cpp
@@ -5,28 +5,29 @@ constexpr int N_ELEMENTS = 100;
 int main()
 {
     // Hianyzik az alulvonas
-    int *b = new int[NELEMENTS];
+    int *b = new int[N_ELEMENTS];
     // Nincsen pontosvesszo a sor vegen, rossz aposztrof  
-    std::cout << '1-100 ertekek duplazasa'
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
     // csak a for ciklus kezdoerteke van
-    for (int i = 0;)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
     // 0-tól 99-ig duplaz... 1től 100-ig lenne helyes    
-        b[i] = i * 2;
+         b[i] = (i + 1) * 2;
     }
     // a ciklus vezerlo hibas, mindig hamis lesz az eredmenye, igy nem lep be a ciklusba
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
     // Nem irjuk ki az erteket, es hianyzik a pontosvesszo    
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     //nincs kezdoerteke az atlag valtozonak
-    int atlag;
+    int atlag = 0;
     //az N_ELEMENTS utan ; kell nem pedig vesszo
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i]
+    // hianyzik egy pontosvesszo    
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
